38443b86112127cb3cb814d4de231bf5180440f5
pavelk03 <72764905+pavelk03@users.noreply.github.com>
2020-11-03 21:17:34 +0100

    Add files via upload

---
+++ Ukol 1_4/src/ukol/pkg1_4/Ukol1_4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ukol.pkg1_4;
+
+/**
+ *
+ * @author Kincl, Přibyl, Pilát 
+ */
+public class Ukol1_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // celočíselné
+       byte breznev;
+       short simon;
+       int igor;
+       long libor;
+       
+       // reálné
+       float filip;
+       double denis;
+       
+       // znak
+       char charlie;
+       
+       // logický
+       boolean bob;
+       
+    }
+    
+}
